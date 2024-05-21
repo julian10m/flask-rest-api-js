69b71fbbfc3b462defe4fc22087b9b768f47a78c
Julian M. Del Fiore <julian.delfiore@team.bumble.com>
2024-05-22 00:09:22 +0200

    add support for registration emails

--- resources/user.py
+++ resources/user.py
@@ -1,30 +1,51 @@
+import os
+import requests
+
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt, get_jwt_identity, jwt_required
 
 from passlib.hash import pbkdf2_sha256
+from sqlalchemy import or_
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 
 from db import db
 from blocklist import BLOCKLIST
 
 from models import UserModel
-from schemas import UserSchema
+from schemas import UserSchema, UserRegisterSchema
+
+def send_simple_message(to, subject, body):
+    domain = os.getenv("MAILGUN_DOMAIN")
+    return requests.post(
+        f"https://api.mailgun.net/v3/{domain}/messages",
+        auth=("api", os.getenv("MAILGUN_API_KEY")),
+        data={"from": f"Pepo <mailgun@{domain}>",
+			"to": [to],
+			"subject": subject,
+			"text": body
+        })
 
 blp = Blueprint("Users", "users", description="Operations on users")
 
 @blp.route("/register")
 class UserRegister(MethodView):
-    @blp.arguments(UserSchema)
+    @blp.arguments(UserRegisterSchema)
     def post(self, user_data):
         user = UserModel(
             username=user_data["username"],
+            email=user_data["email"],
             password=pbkdf2_sha256.hash(user_data["password"]),
         )
 
         try:
             db.session.add(user)
             db.session.commit()
+            mailgun_response = send_simple_message(
+                to=user.email,
+                subject="Successfully signed up.",
+                body=f"Hi {user.username}! You signed up to the REST API."
+            )
         except IntegrityError as e:
             abort(400, message=str(e.orig))
         except SQLAlchemyError:
